1f154b3390e1825fbd34f73c9caa3c03903a0472
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2023-02-02 15:12:52 +0000

    update android
    update android

--- src/android/com/galp/plugins/bipdrive/GalpBipDriveImpl.java
+++ src/android/com/galp/plugins/bipdrive/GalpBipDriveImpl.java
@@ -5,11 +5,15 @@ import android.util.Log;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import com.Bipdrive.Bipdrivecore;
+import com.Bipdrive.Bipdrivecore.Utils;
+import com.Bipdrive.Bipdriveparkingmeter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class GalpArImpl extends CordovaPlugin {
+
+public class GalpBipDriveImpl extends CordovaPlugin {
 
     static private CallbackContext _callbackContext;
 
@@ -17,12 +21,32 @@ public class GalpArImpl extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         _callbackContext = callbackContext;
 
-        if (action.equals("openSDK")) {
+        if (action.equals("launchParkingMeter")) {
 
-            JSONObject params = args.optJSONObject(0);
-            String resourcePath = params.optString("resourcePath");
-            // TODO
-            return true;
+            /**
+            appId: 6^7&AL3rCbWl
+            appKey: LqMY01d1!sbZz^g5Hu5BJOM9Qi75Ws3*6AdmpBV&M9bfvW6BT&
+            clientId: f835071b-529a-4748-a883-cedd1f9c8411
+             */
+            BipDriveConfig bipDriveConfig = new BipDriveConfig(
+                    "LqMY01d1!sbZz^g5Hu5BJOM9Qi75Ws3*6AdmpBV&M9bfvW6BT&",
+                    "6^7&AL3rCbWl",
+                    "f835071b-529a-4748-a883-cedd1f9c8411");
+
+            BDParkingMeterLaunch.INSTANCE.launchParkingMeter(MainActivity.this, bipDriveConfig, new ConfigValidationResult(){
+                @Override
+                public void onValidationOK(@NotNull ConfigValidationResponse configValidationResponse) {
+
+                }
+
+                @Override
+                public void onValidationKO(@NotNull BDErrorResponse bdErrorResponse) {
+                    new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(MainActivity.this,
+                            bdErrorResponse.toString(),
+                            Toast.LENGTH_SHORT).show());
+                }
+            });
+             return true;
         }
         return false;
     }
--- www/GalpBipDriveImpl.js
+++ www/GalpBipDriveImpl.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.openSDK = function (params, success, error) {
     exec(success, error, 'GalpBipDriveImpl', 'openSDK', [params]);
 };
+
+exports.launchParkingMeter = function (params, success, error) {
+    exec(success, error, 'GalpBipDriveImpl', 'launchParkingMeter', [params]);
+};
